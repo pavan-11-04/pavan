a1e576285839e5911cab4a234b5bccd4b2ff70d6
pavan-11-04 <pavan2006r@gmail.com>
2026-07-13 12:21:22 +0530

    13-07-26 In-Build function

---
+++ test.R
@@ -0,0 +1,9 @@
+marks <- c(78, 85, 92, 67, 88)
+marks + 5
+round((marks / 120) * 100, 2)
+internal <- c(20, 25, 18, 22, 24)
+external <- c(58, 60, 74, 45, 64)
+
+total <- internal + external
+cat("Total marks: ")
+print(total)
